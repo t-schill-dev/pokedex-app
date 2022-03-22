ca757747d5a29f3f4770e39d9150e38383fceedb
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-03-22 15:00:00 +0100

    Add loop over array to write name and height

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,12 @@ let pokemonList = [{
     }
 
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    // Distinguish items by height using a conditional
+    if (pokemonList[i].height > 1) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m ) " + "- That's really big!")
+    } else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m )");
+    }
+}
