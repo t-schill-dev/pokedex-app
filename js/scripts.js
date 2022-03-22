9d5315faecb7dba7c32e706310b6b22919feec3f
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-03-22 12:21:04 +0100

    Add first objects to array

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+let pokemonList = [{
+        name: 'Bulbasaur',
+        height: 7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        type: 'fire'
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        type: ['grass', 'poisson']
+    },
+    {
+        name: 'Nidoking',
+        height: 1.4,
+        type: ['ground', 'poison']
+    }
+
+];
