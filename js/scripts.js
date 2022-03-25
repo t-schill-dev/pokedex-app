924e1b1b31de4d9f787ea42e51911771e9e38b77
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-03-25 10:59:06 +0100

    Create IIFE of original array and adjust method

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
-let pokemonList = [{
-        name: 'Bulbasaur',
-        height: 7,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        type: 'fire'
-    },
-    {
-        name: 'Venusaur',
-        height: 2,
-        type: ['grass', 'poisson']
-    },
-    {
-        name: 'Nidoking',
-        height: 1.4,
-        type: ['ground', 'poison']
+let pokemonRepository = (function() {
+
+    let pokemonList = [{
+            name: 'Bulbasaur',
+            height: 7,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            type: 'fire'
+        },
+        {
+            name: 'Venusaur',
+            height: 2,
+            type: ['grass', 'poisson']
+        },
+        {
+            name: 'Nidoking',
+            height: 1.4,
+            type: ['ground', 'poison']
+        }
+    ];
+
+    function add(pokemon) {
+        return pokemonList.push(pokemon);
     }
-];
+
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add,
+        getAll
+    };
+})();
 //Creates list before inner loop
 document.write(
     "<ul class='pokemon-container'>"
 );
-pokemonList.forEach(function(item) {
+pokemonRepository.getAll().forEach(function(item) {
         // Distinguish items by height using a conditional
         if (item.height > 1) {
             document.write(
