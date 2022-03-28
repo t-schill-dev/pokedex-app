b1659a15d8c6995e6446817eb7ccfe691f78e1d6
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-03-28 14:34:14 +0200

    Add event listener to show details

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ let pokemonRepository = (function() {
             type: ['ground', 'poison']
         }
     ];
-
+    // add Pokemon to list and validate its input keys
     function add(pokemon) {
         if (typeof pokemon === 'object' &&
             Object.keys(pokemon).toString() === 'name,height,type') {
@@ -36,7 +36,12 @@ let pokemonRepository = (function() {
         return pokemonList;
     }
 
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
     function addListItem(pokemon) {
+        // Create button for each pokemon passed on
         let pokemonContainer = document.querySelector('ul');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
@@ -44,7 +49,13 @@ let pokemonRepository = (function() {
         button.classList.add('pokemon-item');
         listItem.appendChild(button);
         pokemonContainer.appendChild(listItem);
+        // Show details of clicked Pokemon
     }
+
+    function addEvent(button, pokemon) {
+        button.addEventListener('click', showDetails(pokemon));
+    }
+    // Simplified return statement when key === value e.g. add: add
     return {
         add,
         getAll,
